8f3e2f688ab6c002c00b5cf32b6868aeddd6558c
tuvior <tobiasuzt@gmail.com>
2017-10-12 14:31:38 +0200

    node class

--- deliberative/src/deliberative/DeliberativeAgent.java
+++ deliberative/src/deliberative/DeliberativeAgent.java
@@ -12,13 +12,17 @@ import logist.task.TaskSet;
 import logist.topology.Topology;
 import logist.topology.Topology.City;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * An optimal planner for one vehicle.
  */
-@SuppressWarnings("unused")
 public class DeliberativeAgent implements DeliberativeBehavior {
 
     enum Algorithm {BFS, ASTAR}
+
+    enum Action{MOVE, PICKUP, DELIVER}
 
     /* Environment */
     Topology topology;
@@ -82,6 +86,45 @@ public class DeliberativeAgent implements DeliberativeBehavior {
             // This cannot happen for this simple agent, but typically
             // you will need to consider the carriedTasks when the next
             // plan is computed.
+        }
+    }
+
+    public class Node {
+        public City agentPosition;
+        public Action parentAction;
+        public double weightCarried;
+
+        public Set<Task> tasksCarried;
+        public Set<Task> tasksAvailable;
+
+        public Node parent;
+        public Node next;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Node node = (Node) o;
+
+            if (Double.compare(node.weightCarried, weightCarried) != 0) return false;
+            if (agentPosition != null ? !agentPosition.equals(node.agentPosition) : node.agentPosition != null)
+                return false;
+            if (tasksCarried != null ? !tasksCarried.equals(node.tasksCarried) : node.tasksCarried != null)
+                return false;
+            return tasksAvailable != null ? tasksAvailable.equals(node.tasksAvailable) : node.tasksAvailable == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result;
+            long temp;
+            result = agentPosition != null ? agentPosition.hashCode() : 0;
+            temp = Double.doubleToLongBits(weightCarried);
+            result = 31 * result + (int) (temp ^ (temp >>> 32));
+            result = 31 * result + (tasksCarried != null ? tasksCarried.hashCode() : 0);
+            result = 31 * result + (tasksAvailable != null ? tasksAvailable.hashCode() : 0);
+            return result;
         }
     }
 }
