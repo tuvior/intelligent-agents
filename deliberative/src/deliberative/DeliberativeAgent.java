a3ac14962c95f8a8911758d791e6db8379c31bd9
tuvior <tobiasuzt@gmail.com>
2017-10-12 13:54:46 +0200

    base for agent

---
+++ deliberative/src/deliberative/DeliberativeAgent.java
@@ -0,0 +1,87 @@
+package deliberative;
+
+/* import table */
+
+import logist.agent.Agent;
+import logist.behavior.DeliberativeBehavior;
+import logist.plan.Plan;
+import logist.simulation.Vehicle;
+import logist.task.Task;
+import logist.task.TaskDistribution;
+import logist.task.TaskSet;
+import logist.topology.Topology;
+import logist.topology.Topology.City;
+
+/**
+ * An optimal planner for one vehicle.
+ */
+@SuppressWarnings("unused")
+public class DeliberativeAgent implements DeliberativeBehavior {
+
+    enum Algorithm {BFS, ASTAR}
+
+    /* Environment */
+    Topology topology;
+    TaskDistribution td;
+
+    /* the properties of the agent */
+    Agent agent;
+    int capacity;
+
+    /* the planning class */
+    Algorithm algorithm;
+
+    @Override
+    public void setup(Topology topology, TaskDistribution td, Agent agent) {
+        this.topology = topology;
+        this.td = td;
+        this.agent = agent;
+
+        // initialize the planner
+        int capacity = agent.vehicles().get(0).capacity();
+        String algorithmName = agent.readProperty("algorithm", String.class, "ASTAR");
+
+        // Throws IllegalArgumentException if algorithm is unknown
+        algorithm = Algorithm.valueOf(algorithmName.toUpperCase());
+
+        // ...
+    }
+
+    @Override
+    public Plan plan(Vehicle vehicle, TaskSet tasks) {
+        Plan plan;
+
+        // Compute the plan with the selected algorithm.
+        switch (algorithm) {
+            case ASTAR:
+                plan = planASTAR(vehicle, tasks);
+                break;
+            case BFS:
+                plan = planBFS(vehicle, tasks);
+                break;
+            default:
+                throw new AssertionError("Should not happen.");
+        }
+        return plan;
+    }
+
+    private Plan planBFS(Vehicle vehicle, TaskSet tasks) {
+
+        return null;
+    }
+
+    private Plan planASTAR(Vehicle vehicle, TaskSet tasks) {
+
+        return null;
+    }
+
+    @Override
+    public void planCancelled(TaskSet carriedTasks) {
+
+        if (!carriedTasks.isEmpty()) {
+            // This cannot happen for this simple agent, but typically
+            // you will need to consider the carriedTasks when the next
+            // plan is computed.
+        }
+    }
+}
