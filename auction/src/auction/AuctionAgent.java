3ced0145f8971fa7e51e2ae8b3392d790eccd7b2
tuvior <tobiasuzt@gmail.com>
2017-11-15 13:57:28 +0100

    what we wrote before starting

--- auction/src/auction/AuctionAgent.java
+++ auction/src/auction/AuctionAgent.java
@@ -21,96 +21,111 @@ import java.util.Random;
 
 public class AuctionAgent implements AuctionBehavior {
 
-	private Topology topology;
-	private TaskDistribution distribution;
-	private Agent agent;
-	private Random random;
-	private Vehicle vehicle;
-	private City currentCity;
-	private long timeout_setup;
-	private long timeout_plan;
+    private Topology topology;
+    private TaskDistribution distribution;
+    private Agent agent;
+    private Random random;
+    private Vehicle vehicle;
+    private City currentCity;
+    private long timeout_setup;
+    private long timeout_plan;
+    private long timeout_bid;
 
-	@Override
-	public void setup(Topology topology, TaskDistribution distribution, Agent agent) {
+    private static boolean [] xd  = new boolean [Integer.MAX_VALUE];
 
-		try {
-			LogistSettings ls = Parsers.parseSettings("config/settings_default.xml");
-			timeout_setup = ls.get(LogistSettings.TimeoutKey.SETUP);
-			timeout_plan = ls.get(LogistSettings.TimeoutKey.PLAN);
-		} catch (Exception ex) {
-			System.err.println("There was a problem loading the configuration file.");
-		}
+    @Override
+    public void setup(Topology topology, TaskDistribution distribution, Agent agent) {
 
-		this.topology = topology;
-		this.distribution = distribution;
-		this.agent = agent;
-		this.vehicle = agent.vehicles().get(0);
-		this.currentCity = vehicle.homeCity();
+        try {
+            LogistSettings ls = Parsers.parseSettings("config/settings_default.xml");
+            timeout_setup = ls.get(LogistSettings.TimeoutKey.SETUP);
+            timeout_plan = ls.get(LogistSettings.TimeoutKey.PLAN);
+            timeout_bid = ls.get(LogistSettings.TimeoutKey.BID);
+        } catch (Exception ex) {
+            System.err.println("There was a problem loading the configuration file.");
+        }
 
-		long seed = -9019554669489983951L * currentCity.hashCode() * agent.id();
-		this.random = new Random(seed);
-	}
+        this.topology = topology;
+        this.distribution = distribution;
+        this.agent = agent;
+        this.vehicle = agent.vehicles().get(0);
+        this.currentCity = vehicle.homeCity();
 
-	@Override
-	public void auctionResult(Task previous, int winner, Long[] bids) {
-		if (winner == agent.id()) {
-			currentCity = previous.deliveryCity;
-		}
-	}
-	
-	@Override
-	public Long askPrice(Task task) {
+        long seed = -9019554669489983951L * currentCity.hashCode() * agent.id();
+        this.random = new Random(seed);
+    }
 
-		if (vehicle.capacity() < task.weight)
-			return null;
+    @Override
+    public void auctionResult(Task previous, int winner, Long[] bids) {
+        if (winner == agent.id()) {
+            currentCity = previous.deliveryCity;
+        }
+    }
 
-		long distanceTask = task.pickupCity.distanceUnitsTo(task.deliveryCity);
-		long distanceSum = distanceTask
-				+ currentCity.distanceUnitsTo(task.pickupCity);
-		double marginalCost = Measures.unitsToKM(distanceSum
-				* vehicle.costPerKm());
+    private void evaluateTopology(Topology topology, TaskDistribution distribution) {
+        topology.cities().forEach(city -> {
+            final double[] totalProb1 = {0};
+            final double[] totalProb2 = {0};
+            topology.cities().forEach(city2 -> {
+                totalProb1[0] += distribution.probability(city, city2);
+                totalProb2[0] += distribution.probability(city2, city);
+            });
+        });
+    }
 
-		double ratio = 1.0 + (random.nextDouble() * 0.05 * task.id);
-		double bid = ratio * marginalCost;
+    @Override
+    public Long askPrice(Task task) {
 
-		return (long) Math.round(bid);
-	}
+        if (vehicle.capacity() < task.weight)
+            return null;
 
-	@Override
-	public List<Plan> plan(List<Vehicle> vehicles, TaskSet tasks) {
-		
+        long distanceTask = task.pickupCity.distanceUnitsTo(task.deliveryCity);
+        long distanceSum = distanceTask
+                + currentCity.distanceUnitsTo(task.pickupCity);
+        double marginalCost = Measures.unitsToKM(distanceSum
+                * vehicle.costPerKm());
+
+        double ratio = 1.0 + (random.nextDouble() * 0.05 * task.id);
+        double bid = ratio * marginalCost;
+
+        return (long) Math.round(bid);
+    }
+
+    @Override
+    public List<Plan> plan(List<Vehicle> vehicles, TaskSet tasks) {
+
 //		System.out.println("Agent " + agent.id() + " has tasks " + tasks);
 
-		Plan planVehicle1 = naivePlan(vehicle, tasks);
+        Plan planVehicle1 = naivePlan(vehicle, tasks);
 
-		List<Plan> plans = new ArrayList<Plan>();
-		plans.add(planVehicle1);
-		while (plans.size() < vehicles.size())
-			plans.add(Plan.EMPTY);
+        List<Plan> plans = new ArrayList<Plan>();
+        plans.add(planVehicle1);
+        while (plans.size() < vehicles.size())
+            plans.add(Plan.EMPTY);
 
-		return plans;
-	}
+        return plans;
+    }
 
-	private Plan naivePlan(Vehicle vehicle, TaskSet tasks) {
-		City current = vehicle.getCurrentCity();
-		Plan plan = new Plan(current);
+    private Plan naivePlan(Vehicle vehicle, TaskSet tasks) {
+        City current = vehicle.getCurrentCity();
+        Plan plan = new Plan(current);
 
-		for (Task task : tasks) {
-			// move: current city => pickup location
-			for (City city : current.pathTo(task.pickupCity))
-				plan.appendMove(city);
+        for (Task task : tasks) {
+            // move: current city => pickup location
+            for (City city : current.pathTo(task.pickupCity))
+                plan.appendMove(city);
 
-			plan.appendPickup(task);
+            plan.appendPickup(task);
 
-			// move: pickup location => delivery location
-			for (City city : task.path())
-				plan.appendMove(city);
+            // move: pickup location => delivery location
+            for (City city : task.path())
+                plan.appendMove(city);
 
-			plan.appendDelivery(task);
+            plan.appendDelivery(task);
 
-			// set current city
-			current = task.deliveryCity;
-		}
-		return plan;
-	}
+            // set current city
+            current = task.deliveryCity;
+        }
+        return plan;
+    }
 }
