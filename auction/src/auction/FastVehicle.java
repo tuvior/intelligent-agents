0bd7f6d2dea0461163889739e1291bbb4e54dbcf
paulnicolet <nicoletpaul@orange.fr>
2017-11-16 15:13:40 +0100

    Generate new vehicles

--- auction/src/auction/FastVehicle.java
+++ auction/src/auction/FastVehicle.java
@@ -4,7 +4,10 @@ import logist.simulation.Vehicle;
 import logist.task.TaskSet;
 import logist.topology.Topology;
 
-import java.awt.*;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class FastVehicle implements Vehicle {
     private int id;
@@ -12,6 +15,8 @@ public class FastVehicle implements Vehicle {
     private int costPerKm;
     private String name;
     private Topology.City homeCity;
+
+    public enum HomeCityRandomness {NONE, NEIGHBOR, FULL};
 
     public FastVehicle(Vehicle vehicle) {
         this.id = vehicle.id();
@@ -27,6 +32,62 @@ public class FastVehicle implements Vehicle {
         this.capacity = capacity;
         this.homeCity = homeCity;
         this.costPerKm = costPerKm;
+    }
+
+    public static List<FastVehicle> generateVehicles(List<Vehicle> vehicles,
+                                                     Topology topology,
+                                                     boolean sameNumber,
+                                                     boolean sameCapacity,
+                                                     boolean sameCost,
+                                                     HomeCityRandomness cityRandomness) {
+        List<FastVehicle> newVehicles = new ArrayList<>();
+        Random random = new Random();
+
+        // Generate number of new vehicle
+        int nVehicles = vehicles.size();
+        if (!sameNumber) {
+            nVehicles = random.nextBoolean() ? nVehicles - 1 : nVehicles + 1;
+            nVehicles = Math.max(1, nVehicles);
+        }
+
+        // Generate vehicles
+        for(int i = 0; i < nVehicles; ++i) {
+            Vehicle reference = vehicles.get(i % vehicles.size());
+
+            int capacity = generateValue(reference.capacity(), sameCapacity, 0.2);
+            Topology.City city = generateHomeCity(topology, reference.homeCity(), cityRandomness);
+            int costPerKm = generateValue(reference.costPerKm(), sameCost, 0.2);
+
+            FastVehicle v = new FastVehicle(capacity, city, costPerKm);
+            newVehicles.add(v);
+        }
+
+        return newVehicles;
+    }
+
+    private static int generateValue(int value, boolean same, double scaleWidth) {
+        if (same) return value;
+
+        Random random = new Random();
+        double window = random.nextDouble() * scaleWidth;
+        double factor = random.nextBoolean() ? 1 - window : 1 + window;
+
+        return (int) (factor * value);
+    }
+
+    private static Topology.City generateHomeCity(Topology topology, Topology.City city, HomeCityRandomness randomness) {
+        switch (randomness) {
+            case NONE:
+                return city;
+
+            case NEIGHBOR:
+                return city.randomNeighbor(new Random());
+
+            case FULL:
+                return topology.randomCity(new Random());
+        }
+
+        return city;
     }
 
     @Override
@@ -54,7 +115,7 @@ public class FastVehicle implements Vehicle {
         return this.costPerKm;
     }
 
-    // Unused methods
+    // ----- Unused methods ----- //
     @Override
     public double speed() {
         return Double.MAX_VALUE;
